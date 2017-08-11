186ea18aca20161c1deae60bf8f9dde2239bd652
nicolasbeauvais <nicolasbeauvais1@gmail.com>
2017-08-11 14:33:23 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `flysystem-onedrive` will be documented in this file
+
+## 1.0.0 - 2017-08-11
+
+- initial release
---
+++ tests/OneDriveAdapterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NicolasBeauvais\FlysystemOneDrive\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class OneDriveAdapterTest extends TestCase
+{
+    /** @test */
+    public function it_can_run_tests()
+    {
+        $this->assertTrue(true);
+    }
+}
